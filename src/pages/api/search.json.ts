b1c1245e74dd44c804564f0d7caba64cadea46d1
Ebenezer Don <ebenezerdonu@gmail.com>
2024-12-04 21:43:42 +0000

    Fix lint errors

--- src/pages/api/search.json.ts
+++ src/pages/api/search.json.ts
@@ -1,10 +1,10 @@
 import type { APIRoute } from 'astro'
-import { getCollection } from 'astro:content'
+import { getCollection, type CollectionEntry } from 'astro:content'
 
 export const GET: APIRoute = async () => {
   const docs = await getCollection('docs')
 
-  const searchIndex = docs.map((entry) => ({
+  const searchIndex = docs.map((entry: CollectionEntry<'docs'>) => ({
     title: entry.data.title,
     description: entry.data.description,
     group: entry.data.group,
