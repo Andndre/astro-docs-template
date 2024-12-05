bcca56bd62d767450839f01488bcacc59868a0cc
Ebenezer Don <ebenezerdonu@gmail.com>
2024-12-05 17:03:40 +0000

    Update vercel import

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config'
 import svelte from '@astrojs/svelte'
 import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel'
 
 export default defineConfig({
   output: 'server',
