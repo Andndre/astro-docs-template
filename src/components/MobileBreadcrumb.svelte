4c917d03e9303ddcd2b66b5db9cf15c7e83349ed
Ebenezer Don <ebenezerdonu@gmail.com>
2024-12-09 12:11:18 +0000

    Ensure breadcrumb shows "home" for index page regardless of title

--- src/components/MobileBreadcrumb.svelte
+++ src/components/MobileBreadcrumb.svelte
@@ -5,6 +5,15 @@
   export let group: string | undefined
   export let title: string
 
+  // Get the current path
+  let currentPath = ''
+  if (typeof window !== 'undefined') {
+    currentPath = window.location.pathname
+  }
+
+  // If we're on the index page (root path), always show "Home"
+  $: displayTitle = currentPath === '/' ? 'Home' : title
+
   onMount(() => {
     const sidebarToggle = document.getElementById('sidebar-toggle')
     const leftSidebar = document.getElementById('left-sidebar')
@@ -31,7 +40,7 @@
       {#if group}
         <span>{group} / </span>
       {/if}
-      <span class="text-[#2D2D31] dark:text-white">{title}</span>
+      <span class="text-[#2D2D31] dark:text-white">{displayTitle}</span>
     </div>
   </div>
 </div>
